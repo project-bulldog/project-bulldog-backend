cd4edd56d8cccb041629ba5cc04b61de37fa6684
Zach Sykes <zsykes21@gmail.com>
2025-05-25 07:01:08 -0600

    Fix formatting issue

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
