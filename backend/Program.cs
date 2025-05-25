6dbef5d68ba99ecc1dec0b678e529d4c1de914a1
Zach Sykes <112282023+Calathea-Z@users.noreply.github.com>
2025-05-25 12:49:07 -0600

    Turn on swagger for prod; Add fake route in Program.cs for test. (#4)
    * Turn on swagger for prod; Add fake route in Program.cs for test.
    
    * Update backend CI workflow to specify solution file for restore, build, format, and test commands
    
    * Enhance backend CI workflow by installing dotnet-format tool and updating formatting command
    
    * Update backend CI workflow to change dotnet-format command from --verify-no-changes to --check
    
    * Update backend CI workflow to run tests with Release configuration

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,12 +5,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.MapGet("/", () => "Hello world!");
+app.MapGet("/weather", () =>
+{
+    return new[] {
+        new { Date = DateTime.Now, TempC = 25, Summary = "Sunny" }
+    };
+});
 
 app.Run();
